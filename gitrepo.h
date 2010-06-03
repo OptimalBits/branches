1cd08a4ef7a429d8e2bad1ce95e297a773438627
Manuel Astudillo <manuel@Manuel-Astudillos-MacBook.local>
2010-06-03 21:48:32 +0200

    header for history, and some clean ups.

--- gitrepo.h
+++ gitrepo.h
@@ -7,20 +7,37 @@
 //
 
 #import <Foundation/Foundation.h>
+#include "gitpackfile.h"
+
+typedef char Sha1[40];
+
 
 @interface GitRepo : NSObject {
-	NSString *description;
-			
-	//GitConfig *config;
 	
-	NSURL* url;
+	
 }
 
 @property (retain) NSURL* url;
 @property (readonly) NSMutableDictionary *refs;
 
+//@property (readonly) NSString *description;
+//@property (readonly) GitObject *head;
+//@property (readonly) GitConfig *config;
+
 - (id) initWithUrl: (NSURL*) path;
 - (void) dealloc;
+
+// (void) createHead:
+// (void) createTag:
+// (void) createRemote:
+/// ...
+
+//- (GitCommit*) commit:(Sha1) sha1;
+
+- (NSArray*) revisionHistoryFor:(NSData*) sha1 withPackFile: (GitPackFile*) packFile;
+
+
+// Private func.
 - (void) parseRefs; // Parses refs and pack_refs
 
 
