3fccf521a8fad6eaeee9e019062e846b437dc50a
Manuel Astudillo <manuel@Manuel-Astudillos-MacBook.local>
2010-06-03 21:49:51 +0200

    Added useful extensions for NSData.

---
+++ NSDataExtension.h
@@ -0,0 +1,47 @@
+//
+//  NSDataExtension.h
+//  gitfend
+//
+//  Created by Manuel Astudillo on 5/23/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//  
+
+#import <Foundation/Foundation.h>
+
+@interface NSData (NSDataExtension)
+
+// Returns range [start, null byte), or (NSNotFound, 0).
+- (NSRange) rangeOfNullTerminatedBytesFrom:(int)start;
+
+// Canonical Base32 encoding/decoding.
++ (NSData *) dataWithBase32String:(NSString *)base32;
+- (NSString *) base32String;
+
++ (NSData *) dataWithHexCString:(const char *) hex;
++ (NSData *) dataWithHexString:(NSString *) hex;
+
+
+// COBS is an encoding that eliminates 0x00.
+- (NSData *) encodeCOBS;
+- (NSData *) decodeCOBS;
+
+// ZLIB
+- (NSData *) zlibInflate;
+- (NSData *) zlibDeflate;
+
+// GZIP
+- (NSData *) gzipInflate;
+- (NSData *) gzipDeflate;
+
+//CRC32
+- (unsigned int)crc32;
+
+// Hash
+- (NSData*) md5Digest;
+- (NSString*) md5DigestString;
+- (NSData*) sha1Digest;
+- (NSString*) sha1DigestString;
+- (NSData*) ripemd160Digest;
+- (NSString*) ripemd160DigestString;
+
+@end
