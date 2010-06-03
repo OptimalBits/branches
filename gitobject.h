cd6ff9bed35548a2f6b26b85ad47927bab158aae
Manuel Astudillo <manuel@Manuel-Astudillos-MacBook.local>
2010-06-03 21:52:40 +0200

    Added the GitObject class.

---
+++ gitobject.h
@@ -0,0 +1,23 @@
+//
+//  gitobject.h
+//  gitfend
+//
+//  Created by Manuel Astudillo on 5/20/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface GitObject : NSObject {
+
+}
+
+//@property (readonly) NSStream *dataStream;
+//@property (readwrite,assign) NSString *sha1;
+//@property (readwrite,assign) NSURL *repo;
+
+//- (id) writeObject: (NSStream*) stream;
+
+
+@end
---
+++ gitobject.m
@@ -0,0 +1,18 @@
+//
+//  gitobject.m
+//  gitfend
+//
+//  Created by Manuel Astudillo on 5/20/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "gitobject.h"
+
+
+
+
+@implementation GitObject
+
+
+
+@end
