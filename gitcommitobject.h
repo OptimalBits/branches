3204840361cd38e113da437595c4750f64d6e79d
Manuel Astudillo <manuel@Manuel-Astudillos-MacBook.local>
2010-06-03 21:52:56 +0200

    Added the GitCommitObject

---
+++ gitcommitobject.h
@@ -0,0 +1,42 @@
+//
+//  gitcommitobject.h
+//  gitfend
+//
+//  Created by Manuel Astudillo on 5/22/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "gitobject.h"
+
+@interface GitAuthor : NSObject
+{
+
+}
+
+@property (readwrite, retain) NSString *name;
+@property (readwrite, retain) NSString *email;
+@property (readwrite, retain) NSDate *time;
+@property (readwrite, retain) NSString *offutc;
+
+-(id) initWithName: name email: email andTime: time;
+
+
+@end
+
+
+@interface GitCommitObject : GitObject 
+{
+}
+
+@property (readwrite, retain) NSData *tree;
+@property (readwrite, retain) NSMutableArray *parents;
+@property (readwrite, retain) NSString *message;
+@property (readwrite, retain) NSData *sha1;
+@property (readwrite, retain) GitAuthor *author;
+@property (readwrite, retain) GitAuthor *committer;
+
+
+- (id) initWithData: (NSData*) data sha1: (NSData*) sha1;
+
+@end
---
+++ gitcommitobject.m
@@ -0,0 +1,112 @@
+//
+//  gitcommitobject.m
+//  gitfend
+//
+//  Created by Manuel Astudillo on 5/22/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "gitcommitobject.h"
+#import "RegexKitLite.h"
+#import "NSDataExtension.h"
+
+@implementation GitAuthor
+
+@synthesize name;
+@synthesize email;
+@synthesize time;
+@synthesize offutc;
+
+-(id) initWithName: _name email: _email andTime: _time
+{
+	if ( self = [super init] )
+	{
+		[self setName:_name];
+		[self setEmail:_email];
+		[self setTime:[NSDate dateWithTimeIntervalSince1970:[_time doubleValue]]];
+	}
+	
+	return self;
+}
+
+
+@end
+
+@implementation GitCommitObject
+
+@synthesize tree;
+@synthesize author;
+@synthesize committer;
+@synthesize parents;
+@synthesize message;
+@synthesize sha1;
+
+- (id) initWithData: (NSData*) data sha1: (NSData*) key
+{
+	NSArray *matches;
+	int count;
+
+	if ( self = [super init] )
+    {
+		const char *_sha1;
+		
+		NSString *msgRegExp = @"\n\n((?s:.*))";
+		
+		NSString *regExpTree = @"tree\\s([0-9a-f]{40})";
+		NSString *regExpParent = @"parent\\s([0-9a-f]{40})";
+		NSString *regExpAuthor = @"author\\s(.*)\\s(<(.*)>)\\s([0-9]+)\\s(((\\+)|(\\-))[0-9]+)";
+		NSString *regExpCommitter = @"committer\\s(.*)\\s(<(.*)>)\\s([0-9]+)\\s(((\\+)|(\\-))[0-9]+)";
+		
+		NSString *commitString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+		
+		[self setSha1:key];
+		
+		_sha1 = [[commitString stringByMatching:regExpTree capture:1L] cStringUsingEncoding:NSUTF8StringEncoding];
+		tree = [NSData dataWithHexCString:_sha1];
+		
+		matches = [commitString arrayOfCaptureComponentsMatchedByRegex:regExpParent];
+		count = [matches count];
+		
+		parents = [[NSMutableArray alloc] init];
+		for( NSArray *parentMatch in matches )
+		{
+			_sha1 = [[parentMatch objectAtIndex:1] cStringUsingEncoding:NSUTF8StringEncoding];
+			[parents addObject:[NSData dataWithHexCString:_sha1]];
+		}
+		
+		matches = [commitString arrayOfCaptureComponentsMatchedByRegex:regExpAuthor];
+		count = [matches count];
+		if ( count > 0 )
+		{
+			NSString *name = [[matches objectAtIndex:0] objectAtIndex:1];
+			NSString *email = [[matches objectAtIndex:0] objectAtIndex:3];
+			NSString *time = [[matches objectAtIndex:0] objectAtIndex:4];
+		//	NSString *offutc = [[matches objectAtIndex:0] objectAtIndex:5];
+			
+			author = [[GitAuthor alloc] initWithName: name email: email andTime: time];
+		}
+		
+		matches = [commitString arrayOfCaptureComponentsMatchedByRegex:regExpCommitter];
+		count = [matches count];
+		if ( count > 0 )
+		{
+			NSString *name = [[matches objectAtIndex:0] objectAtIndex:1];
+			NSString *email = [[matches objectAtIndex:0] objectAtIndex:3];
+			NSString *time = [[matches objectAtIndex:0] objectAtIndex:4];
+		//	NSString *offutc = [[matches objectAtIndex:0] objectAtIndex:5];
+			
+			committer = [[GitAuthor alloc] initWithName: name email: email andTime: time];
+		}
+		
+		matches = [commitString arrayOfCaptureComponentsMatchedByRegex:msgRegExp];
+		count = [matches count];
+		if ( count > 0 )
+		{
+			[self setMessage:[NSString stringWithString:[commitString stringByMatching:msgRegExp capture:1L]]];
+		}
+	}
+	
+	return self;
+}
+
+@end
