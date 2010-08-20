81f67f2541d11ba255f073e21b608351291e254c
Manuel Astudillo <manuel@Manuel-Astudillos-MacBook.local>
2010-08-20 10:55:44 +0200

    Added the git tag object.

---
+++ GitTagObject.h
@@ -0,0 +1,16 @@
+//
+//  GitTagObject.h
+//  gitfend
+//
+//  Created by Manuel Astudillo on 8/19/10.
+//  Copyright 2010 CodeTonic. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface GitTagObject : NSObject {
+
+}
+
+@end
---
+++ GitTagObject.m
@@ -0,0 +1,14 @@
+//
+//  GitTagObject.m
+//  gitfend
+//
+//  Created by Manuel Astudillo on 8/19/10.
+//  Copyright 2010 CodeTonic. All rights reserved.
+//
+
+#import "GitTagObject.h"
+
+
+@implementation GitTagObject
+
+@end
