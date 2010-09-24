e1edbd8e1781ac79926d618086b065d42479d893
Manuel Astudillo <manuel@Manuel-Astudillos-MacBook.local>
2010-09-24 16:36:14 +0200

    cleanups.

--- gitfendAppDelegate.m
+++ gitfendAppDelegate.m
@@ -16,26 +16,7 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification 
 {
-	NSError *error;
 	
-	NSLog(@"TESTING CCDiff");
-	
-	NSString *srcA = [NSString stringWithContentsOfFile:@"/Users/manuel/dev/gitfend/GitFrontTree.h" 
-											   encoding:NSASCIIStringEncoding
-												  error:&error];
-	
-	NSString *srcB = [NSString stringWithContentsOfFile:@"/Users/manuel/dev/gitfend/GitFrontTree.m" 
-											   encoding:NSASCIIStringEncoding
-												  error:&error];
-	
-	CCDiff *diff = [[CCDiff alloc] initWithBefore:srcA andAfter:srcB];
-	
-	NSArray *lcs = [diff diff];
-	
-	for( CCDiffLine *line in lcs )
-	{
-		NSLog(@"%@ %@", [line status] == kLineRemoved? @"-":([line status] == kLineAdded?@"+":@""), [line line]);
-	}
 }
 
 -(void) dealloc
