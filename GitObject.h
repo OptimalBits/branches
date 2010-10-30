18f68e796b3e4e59eb641af59bfe9bba06a911e7
Manuel Astudillo <manuel@codetonic.com>
2010-10-30 09:52:19 +0200

    Added encoding functionality.

--- GitObject.h
+++ GitObject.h
@@ -3,14 +3,14 @@
 //  gitfend
 //
 //  Created by Manuel Astudillo on 5/20/10.
-//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//  Copyright 2010 CodeTonic. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
 
 
 @interface GitObject : NSObject {
-
+	NSString *type;
 }
 
 //@property (readonly) NSStream *dataStream;
@@ -19,14 +19,26 @@
 
 //- (id) writeObject: (NSStream*) stream;
 
+-(id) initWithType:(NSString*) type;
+
 /**
 	Returns a serialized version of the object in a NSData object.
+	Note: This method should be overrided by the subclass.
  */
-//-(NSData*) data;
+-(NSData*) data;
 
 /**
 	Returns the sha1 key associated to this object.
+	Note: This method usually will not be overrided. 
  */
-//-(NSData*) sha1;
+-(NSData*) sha1;
+
+/**
+	Returns the encoded data according to git format for objects.
+	Note: This method usually will not be overrided.
+*/
+-(NSData*) encode;
+
+//-(NSData*) sha1WithContent:(NSData*) content;
 
 @end
--- GitObject.m
+++ GitObject.m
@@ -3,23 +3,68 @@
 //  gitfend
 //
 //  Created by Manuel Astudillo on 5/20/10.
-//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//  Copyright 2010 CodeTonic. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
 #import "GitObject.h"
+#import "NSDataExtension.h"
 
 @implementation GitObject
-/*
+
+
+-(id) initWithType:(NSString*) _type
+{
+	if ( self = [super init] )
+	{
+		type = _type;
+		[type retain];
+	}
+	
+	return self;
+}
+
+-(void) dealloc
+{
+	[type release];
+	[super dealloc];
+}
+
 -(NSData*) data
 {
 	return nil;
 }
-
+/*
 -(NSData*) sha1
 {
 	return nil;
 }
-*/
+ */
+
+-(NSData*) encode
+{
+	NSData *content = [self data];
+	
+	NSString *header = 
+		[NSString stringWithFormat:@"%@ %d", type, [content length]];
+	
+	const char* cString = [header UTF8String];
+	
+	NSMutableData *data = [NSMutableData dataWithCapacity:
+						   [header length] + [content length] + 1];
+	
+	// length is wrong here, it is characters not number of bytes!
+	[data appendData:[NSData dataWithBytes:cString length:[header length] + 1]];
+	[data appendData:content];
+	
+	return data;
+}
+
+-(NSData*) sha1
+{
+	return [[self encode] sha1Digest];
+}
+
+
 @end
 
